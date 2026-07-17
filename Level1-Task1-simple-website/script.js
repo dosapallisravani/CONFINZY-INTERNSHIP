7396bd80247dc92aff772f919c46842bf576c426
dosapallisravani <dosapallisravani87@gmail.com>
2026-07-17 15:07:46 +0530

    Update script.js

--- Level1-Task1-simple-website/script.js
+++ Level1-Task1-simple-website/script.js
@@ -1,8 +1,8 @@
-
 // Mobile Menu Toggle
 
 const menu = document.querySelector(".menu");
 const navLinks = document.querySelector(".nav-links");
+
 
 menu.addEventListener("click", () => {
 
@@ -12,7 +12,7 @@ menu.addEventListener("click", () => {
 
 
 
-// Close menu after clicking a link
+// Close menu after clicking link
 
 document.querySelectorAll(".nav-links a").forEach(link => {
 
@@ -27,21 +27,22 @@ document.querySelectorAll(".nav-links a").forEach(link => {
 
 
 
-// Scroll Animation
+
+// Scroll Reveal Animation
 
 const sections = document.querySelectorAll("section");
 
 
-const revealSection = () => {
+function reveal(){
 
     sections.forEach(section => {
 
-        const sectionTop = section.getBoundingClientRect().top;
+        const top = section.getBoundingClientRect().top;
 
-        const screenHeight = window.innerHeight;
+        const height = window.innerHeight;
 
 
-        if(sectionTop < screenHeight - 100){
+        if(top < height - 120){
 
             section.classList.add("show");
 
@@ -49,72 +50,96 @@ const revealSection = () => {
 
     });
 
-};
+}
 
 
-window.addEventListener("scroll", revealSection);
+window.addEventListener("scroll", reveal);
 
-revealSection();
-
-
+reveal();
 
 
-// Navbar Background Change
+
+
+
+
+// Navbar Effect on Scroll
 
 const header = document.querySelector("header");
 
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll",()=>{
+
 
     if(window.scrollY > 50){
 
-        header.style.background = "rgba(250,246,239,0.95)";
+        header.style.background =
+        "rgba(250,248,242,0.95)";
 
     }
+
     else{
 
-        header.style.background = "transparent";
+        header.style.background =
+        "transparent";
 
     }
+
 
 });
 
 
 
 
-// Button Smooth Scroll
+
+
+
+// Smooth Scroll
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
-    anchor.addEventListener("click", function(e){
+
+    anchor.addEventListener("click",function(e){
+
 
         e.preventDefault();
 
-        document.querySelector(this.getAttribute("href"))
-        .scrollIntoView({
+
+        document.querySelector(
+        this.getAttribute("href")
+        ).scrollIntoView({
 
             behavior:"smooth"
 
         });
 
+
     });
+
 
 });
 
 
 
 
-// Contact Form Message
+
+
+// Contact Form
 
 const form = document.querySelector("form");
 
 
 form.addEventListener("submit",(e)=>{
 
+
     e.preventDefault();
 
-    alert("Thank you for contacting Aurevia! We will get back to you soon.");
+
+    alert(
+    "Thank you for contacting Aurex! We will get back to you soon."
+    );
+
 
     form.reset();
+
 
 });
